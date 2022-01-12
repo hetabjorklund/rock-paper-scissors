eb0ae6575f884c130ccd235b2a7a430a1a499d1f
Heta Björklund <heta.bjorklund@gmail.com>
2022-01-12 18:50:07 +0200

    App.js is ready

--- src/App.js
+++ src/App.js
@@ -2,20 +2,10 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
 import Past from './components/Past';
 import Current from './components/Current';
 
-function App() {
-  const [value, setValue] = React.useState('1');
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+function App() {  
 
   return (
     <div className="App">
@@ -28,18 +18,8 @@ function App() {
         </Toolbar>
       </AppBar>
 
-      <Box sx={{ width: '100%', typography: 'body1' }}>
-        <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <TabList onChange={handleChange} >
-          {/*    <Tab label="Current games" value="1" /> */}
-           <Tab label="Past games" value="1" /> 
-            </TabList>
-          </Box>
-          {/* <TabPanel value="1"> <Current /> </TabPanel> */}
-            <TabPanel value="1"> <Past /> </TabPanel> 
-        </TabContext>
-        </Box>
+      <Current /> 
+      <Past /> 
 
     </div>
   );
